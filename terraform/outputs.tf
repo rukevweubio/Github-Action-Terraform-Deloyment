816566508911fe03423f0c1da73578eb1e7b61ee
rukevweubio22 <ubiorukevwe@gmail.com>
2025-08-06 08:23:53 +0000

    update teh script

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -15,10 +15,7 @@ output "instance_ids" {
 
 
 
-output "private_key_pem" {
-  value     = tls_private_key.example.private_key_pem
-  sensitive = true
-}
+
 
 output "ec2_public_ip" {
   value = aws_instance.web.public_ip
